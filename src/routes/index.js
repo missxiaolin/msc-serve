05dec60e19fe8d97262443ccf89d7f94569a28dc
xiaolin <462441355@qq.com>
2021-11-03 17:57:40 +0800

    * 登录中间件使用

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,15 @@
 var express = require('express')
-var router = express.Router()
-
+import login from '../middleware/login'
 import RouterConfigBuilder from '../library/utils/router_config_builder'
 import Api from './api'
 
 // 路由分为以下部分
+const baseRouter = express.Router()
+
+// 不需要登录
+const withoutLoginRouter = express.Router()
+// 需要登录
+const loginRouter = express.Router()
 
 let routerConfigMap = {
   ...Api
@@ -12,16 +17,17 @@ let routerConfigMap = {
 
 /**
  * 根据请求方法注册路由
+ * @param {*} customerRouter
  * @param {*} routerConfig
  * @param {*} url
  */
- function registerRouterByMethod(routerConfig, url) {
+function registerRouterByMethod(customerRouter, routerConfig, url) {
   switch (routerConfig.methodType) {
     case RouterConfigBuilder.METHOD_TYPE_GET:
-      router.get(url, routerConfig.func)
+      customerRouter.get(url, routerConfig.func)
       break
     case RouterConfigBuilder.METHOD_TYPE_POST:
-      router.post(url, (req, res) => {
+      customerRouter.post(url, (req, res) => {
         return routerConfig.func(req, res)
       })
       break
@@ -30,16 +36,30 @@ let routerConfigMap = {
 }
 
 
-/* GET home page. */
-router.get('/', function (req, res) {
-  res.json({ title: '根路径' })
-})
+
+
+// 注册路由中间件, 需要在注册路由地址之前使用
+loginRouter.use(login)
 
 // 自动注册
 for (let url of Object.keys(routerConfigMap)) {
   let routerConfig = routerConfigMap[url]
-  // 不需要登录
-  registerRouterByMethod(routerConfig, url)
+  if (routerConfig.needLogin) { // 需要登录
+    registerRouterByMethod(loginRouter, routerConfig, url)
+  } else {  // 不需要登录
+    registerRouterByMethod(withoutLoginRouter, routerConfig, url)
+  }
 }
 
-export default router
+/* GET home page. */
+withoutLoginRouter.get('/', function (req, res) {
+  res.json({ title: '根路径' })
+})
+
+// 处理逻辑为: 从上到下, 依次处理
+baseRouter.use('/', withoutLoginRouter)
+baseRouter.use('/', loginRouter)
+
+
+
+export default baseRouter
