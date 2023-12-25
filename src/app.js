c6ea486ba470923e94a8c99de7c21c125b831d14
lingongbing <462441355@qq.com>
2023-12-25 16:24:27 +0800

    * add

--- src/app.js
+++ src/app.js
@@ -8,92 +8,113 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 */
-import '@babel/polyfill'
+import "@babel/polyfill";
 
-process.env.TZ = 'Asia/Shanghai';
+process.env.TZ = "Asia/Shanghai";
 
-import path from 'path'
-import createError from 'http-errors'
-import express from 'express'
-import cookieParser from 'cookie-parser'
-import morgan from 'morgan'
-import router from './routes/index'
-import cors from 'cors'
-import appConfig from './config/app'
-import bodyParser from 'body-parser'
-import _ from 'lodash'
-import ejs from 'ejs'
+import path from "path";
+import createError from "http-errors";
+import express from "express";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import router from "./routes/index";
+import cors from "cors";
+import appConfig from "./config/app";
+import bodyParser from "body-parser";
+import _ from "lodash";
+import ejs from "ejs";
 
 const serve = require("koa-static-server");
 
 const startup = () => {
-  const app = express()
+  const app = express();
 
   // view engine setup
-  app.set('views', path.join(__dirname, 'views'));
-  
+  app.set("views", path.join(__dirname, "views"));
+
   // 设置模板引擎为ejs
-  app.engine('html', ejs.__express);
+  app.engine("html", ejs.__express);
   // app.engine('html', ejs.renderFile)
-  app.set('view engine', 'html')
+  app.set("view engine", "html");
   // app.set('view engine', 'ejs')
 
-  app.use(morgan('dev'))
+  app.use(morgan("dev"));
   // app.use(express.json())
   // app.use(express.urlencoded({ extended: false }))
-  app.use(bodyParser.urlencoded({ "limit": "10000000kb" }))
+  app.use(bodyParser.urlencoded({ limit: "10000000kb" }));
   // 设置body-parser
   // app.use(bodyParser.urlencoded({ extended: false }))
   // 解析json请求
   // app.use(bodyParser.json({ extended: false }))
-  app.use(bodyParser.json({ "limit": "10000000kb" }))
+  app.use(bodyParser.json({ limit: "10000000kb" }));
   // 设置cookie-parse
-  app.use(cookieParser())
+  app.use(cookieParser());
 
   /* 添加静态路径 */
-  app.use(express.static(path.join(__dirname, './static')))
+  // app.use(express.static(path.join(__dirname, './views/static')))
 
-  /**
- * 创建静态文件
- */
-// app.use(koaStatic(__dirname + '/views'));
-  // app.use(serve({ rootDir: "static", rootPath: "/monitor/upload/static" }));
-  // app.use(serve({ rootDir: "dist" }));
+  app.use(
+    express.static(path.join(__dirname, "./views/static"), {
+      etag: false,
+      setHeaders: (res, path, stat) => {
+        res.set("Cache-Control", "no-store");
+      },
+    })
+  );
 
   // app.options('*', cors())
 
-  app.all('*', function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header('Access-Control-Allow-Methods', '*');
-    res.header('Content-Type', 'application/json;charset=utf-8');
+  app.all("*", function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Content-Type", "application/json;charset=utf-8");
     next();
   });
 
   // 支持跨域
-  app.use(cors({
-    origin: true,
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-    credentials: true
-  }))
+  app.use(
+    cors({
+      origin: true,
+      methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
+      credentials: true,
+    })
+  );
 
-  app.use('/', async (req, res, next) => {
-    let path = req.path
+  app.use("/", async (req, res, next) => {
+    let path = req.path;
     // 只对以 /api & /project/${projectId}/api 路径开头的接口进行响应
-    let projectApiReg = /^\/project\/\d+\/api/i
-    if (_.startsWith(path, '/api') || _.startsWith(path, '/adm') || path.search(projectApiReg) === 0) {
-      return router(req, res, next)
+    let projectApiReg = /^\/project\/\d+\/api/i;
+    if (
+      _.startsWith(path, "/api") ||
+      _.startsWith(path, "/adm") ||
+      path.search(projectApiReg) === 0
+    ) {
+      return router(req, res, next);
     } else {
-      next()
+      next();
     }
-  })
+  });
 
   // 支持前端History模式 => https://router.vuejs.org/zh/guide/essentials/history-mode.html#后端配置例子
   // 将所有404页面均返回index.html
-  app.use('*', (req, res) => {
-    res.type('html')
-    res.render('index')
-  })
+  // 设置正确的 MIME 类型
+  app.use((req, res, next) => {
+    const ext = path.extname(req.url);
+    if (ext === ".js") {
+      res.type("application/javascript");
+    } else if (ext === ".css") {
+      res.type("text/css");
+    } else {
+      res.type("html");
+    }
+    next();
+  });
+  app.use("*", (req, res) => {
+    
+    res.render("index");
+    // res.sendFile(path.join(__dirname, 'dist/index.html'));
+  });
   // app.use('*', (req, res) => {
   //   res.json({
   //     success: false,
@@ -110,21 +131,19 @@ const startup = () => {
   // error handler
   app.use(function (err, req, res, next) {
     // set locals, only providing error in development
-    res.locals.message = err.message
-    res.locals.error = req.app.get('env') === 'development' ? err : {}
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
     // render the error page
-    res.status(err.status || 500)
-    res.render('error')
-  })
+    res.status(err.status || 500);
+    res.render("error");
+  });
 
-  app.set('port', appConfig.port)
+  app.set("port", appConfig.port);
 
   app.listen(appConfig.port, function () {
-    console.log(`${appConfig.name} listening on port ${appConfig.port}`)
-  })
+    console.log(`${appConfig.name} listening on port ${appConfig.port}`);
+  });
+};
 
-}
-
-
-startup()
+startup();
--- src/views/static/usePagination-a31d1b37.js
+++
@@ -1,1 +0,0 @@
-import{f as e}from"./index-ff3f8bb3.js";const a={total:0,currentPage:1,pageSizes:[10,20,50],pageSize:10,layout:"total, sizes, prev, pager, next, jumper"};function t(t,n={}){const r=e({...a,...n});return{paginationData:r,handleCurrentChange:e=>{r.currentPage=e,t()},handleSizeChange:e=>{r.pageSize=e,t()}}}export{t as u};
