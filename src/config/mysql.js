a20e70ffcba80c2a261d3c3b12b2ea8796fd1a14
lingongbing <462441355@qq.com>
2023-12-29 13:14:07 +0800

    * 部署

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -9,7 +9,9 @@ const development = {
     /* nomal */
     host: '127.0.0.1',
     port: '3333',
+    // port: '3306',
     user: 'root',
+    // password: 'root',
     password: 'XiaohongshuMaterial',
     database: 'msc_log'
 }
