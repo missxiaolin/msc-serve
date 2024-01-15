ddf02a0512282561ff21a6256c0263cec6e68969
lingongbing <462441355@qq.com>
2024-01-15 10:43:57 +0800

    * add

---
+++ src/views/static/index-7ae6524b.js
@@ -0,0 +1,1 @@
+import{r as t}from"./service-b1cc4e2f.js";function r(r){return t({url:"/adm/project/save",method:"post",data:r})}function a(r){return t({url:"/adm/project/list",method:"post",data:r})}function o(){return t({url:"/adm/project/all/status/list",method:"post"})}export{a,o as b,r as p};
