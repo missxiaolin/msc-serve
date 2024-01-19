21cf271ea9e3feb376276e28df02afe89d8f70dc
lingongbing <462441355@qq.com>
2024-01-19 14:31:15 +0800

    * 完成

---
+++ src/views/static/index-96f65987.js
@@ -0,0 +1,2 @@
+import{r as a}from"./service-f9d7d744.js";function t(t){return a({url:"/adm/sourcemap/list",method:"post",data:t})}function r(t){return a({url:"/adm/sourcemap/version/list",method:"post",data:t})}function s(t){return a({url:"/adm/sourcemap/analysis",method:"post",data:t})}export{r as a,t as b,s};
+//# sourceMappingURL=index-96f65987.js.map
--- src/views/static/usePagination-346beb36.js
+++
@@ -1,2 +0,0 @@
-import{g as e}from"./index-c4490d7a.js";const a={total:0,currentPage:1,pageSizes:[10,20,50],pageSize:10,layout:"total, sizes, prev, pager, next, jumper"};function t(t,n={}){const r=e({...a,...n});return{paginationData:r,handleCurrentChange:e=>{r.currentPage=e,t()},handleSizeChange:e=>{r.pageSize=e,t()}}}export{t as u};
-//# sourceMappingURL=usePagination-346beb36.js.map
