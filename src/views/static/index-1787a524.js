a8f7940d4fad70892acbd57bf3e8179a02af833f
lingongbing <462441355@qq.com>
2024-01-19 16:19:22 +0800

    * 打包

--- src/views/static/index-1787a524.js
+++
@@ -1,2 +0,0 @@
-import{r as t}from"./service-f9d7d744.js";function r(r){return t({url:"/adm/project/save",method:"post",data:r})}function a(r){return t({url:"/adm/project/list",method:"post",data:r})}function o(){return t({url:"/adm/project/all/status/list",method:"post"})}export{a,o as b,r as p};
-//# sourceMappingURL=index-1787a524.js.map
