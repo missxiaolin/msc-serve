f91dd0172270fdd8a22192ec3266974bdffc3c9a
lingongbing <462441355@qq.com>
2024-01-26 14:30:06 +0800

    * fix

---
+++ src/views/static/index-37590925.js
@@ -0,0 +1,2 @@
+import{r as t}from"./service-53f85039.js";function r(r){return t({url:"/adm/project/save",method:"post",data:r})}function a(r){return t({url:"/adm/project/list",method:"post",data:r})}function o(){return t({url:"/adm/project/all/status/list",method:"post"})}export{a,o as b,r as p};
+//# sourceMappingURL=index-37590925.js.map
--- src/views/static/index-8c1959ab.js
+++
@@ -1,2 +0,0 @@
-import{r as t}from"./service-baf63037.js";function r(r){return t({url:"/adm/project/save",method:"post",data:r})}function a(r){return t({url:"/adm/project/list",method:"post",data:r})}function o(){return t({url:"/adm/project/all/status/list",method:"post"})}export{a,o as b,r as p};
-//# sourceMappingURL=index-8c1959ab.js.map
